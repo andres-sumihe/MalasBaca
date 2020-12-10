a37f2468ded362becf5ffc5d1cacb513cb8a657c
andres-sumihe <672018136@student.uksw.edu>
2020-12-10 11:29:55 +0900

    run dev

--- resources/views/loginPage.blade.php
+++ resources/views/loginPage.blade.php
@@ -2,11 +2,12 @@
 <html>
 <head>
 	<title>MalesBaca</title>
+	<link href="/css/app.css" rel="stylesheet">
 </head>
 <body>
 	<form method="post" action="{{ route('loginCheck') }}">
 		{{ csrf_field() }}
-		<table>
+		<table class="">
 			<tr>
 				<td>NIM</td>
 				<td><input type="text" name="nim" required></td>
@@ -21,5 +22,7 @@
 			</tr>
 		</table>
 	</form>
+
+	<script src="/js/app.js"></script>
 </body>
 </html>
