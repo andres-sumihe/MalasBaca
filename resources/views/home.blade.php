bb392a94e6032b0a555316d767f545de68767be8
audricimanuel <672018407@student.uksw.edu>
2020-12-10 22:18:53 +0800

    cari buku

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -129,7 +129,47 @@
                         Daftar-Pinjam
                 </div>
                 <div class="col-md-12 col-lg-12 tab-pane fade show " id="Cari-Buku" role="tabpanel" aria-labelledby="Cari-Buku-tab" >
-                        Cari-Buku
+                        <div class="container">
+                            <h3><strong>Cari Buku</strong></h3>
+                            <div class="row">
+                                <form method="post" action="{{ route('cariBukuResult') }}">
+                                    {{ csrf_field() }}
+                                    <table>
+                                        <tr>
+                                            <td><input type="text" name="input" placeholder="Cari Buku" required></td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <select name="buku" id="buku">
+                                                  <option value="" selected="true" disabled="true">--Filter Berdasarkan--</option>
+                                                  <option value="nama">Nama Buku</option>
+                                                  <option value="penulis">Penulis Buku</option>
+                                                  <option value="penerbit">Penerbit Buku</option>
+                                                </select>
+                                            </td>
+                                        </tr>
+                                        <tr>                                    
+                                        <tr>
+                                            <td><input type="submit" value="Cari Buku" required></td>
+                                        </tr>
+                                    </table>
+                                </form>
+                                <?php if (isset($resultBuku)): ?>
+                                    <table border="1">
+                                        <tr>
+                                            <th>Nama Buku</th>
+                                            <th>Action</th>
+                                        </tr>
+                                        <?php foreach ($resultBuku as $rB): ?>
+                                            <tr>
+                                                <td>{{ $rB->nama_buku }}</td>
+                                                <td><a href="">Detail</a>
+                                            </tr>
+                                        <?php endforeach ?>
+                                    </table>
+                                <?php endif ?>
+                            </div>
+                        </div>
                 </div>
                 <div class="col-md-12 col-lg-12 tab-pane fade show " id="Daftar-Buku" role="tabpanel" aria-labelledby="Daftar-Buku-tab" >
                     <div class="container">
